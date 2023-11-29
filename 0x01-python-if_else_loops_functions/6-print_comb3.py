1d93f099101cc0c5faa959650ae1dc7d6ed4c634
Aymen21916 <aymenhamidi111@gmail.com>
2023-11-29 00:36:38 -0800

    fer

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if int(str(i)+str(j))%11!=0 and int(str(i)+str(j))<int(str(j)+str(i)):
-            if int(str(i) + str(j)) == 89:
-                print(f"{i}{j}")
-                continue
-            print(f"{i}{j}, ", end='')
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
