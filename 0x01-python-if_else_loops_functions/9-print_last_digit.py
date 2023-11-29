1dc74710a550e97727daa3e1a2f384e305f5f878
Aymen21916 <aymenhamidi111@gmail.com>
2023-11-29 01:21:56 -0800

    fre

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10
+    if number < 0:
+        number *= -1
+        mod = number % 10
+        number *= -1
+    else:
+        mod = number % 10
+    print(mod, end='')
+    return mod
