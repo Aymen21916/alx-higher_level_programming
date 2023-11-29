f841d1a975dae6cc075d58bc5620348c2b5582a8
Aymen21916 <aymenhamidi111@gmail.com>
2023-11-29 01:10:27 -0800

    fre

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    for s in str:
+        if s >= 'a' and s <= 'z':
+            upper += chr(ord(s) - ord('a') + ord('A'))
+        else:
+            upper += s
+    print(upper)
