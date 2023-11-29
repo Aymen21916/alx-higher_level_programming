72b71c1ad147de7a4087a7e2abd950b2f938a0f0
Aymen21916 <aymenhamidi111@gmail.com>
2023-11-29 02:28:39 -0800

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i).upper(), end='')
+    print("{}".format(chr(i)) if i % 2 == 0 else "{}".format(chr(i).upper), end='')
