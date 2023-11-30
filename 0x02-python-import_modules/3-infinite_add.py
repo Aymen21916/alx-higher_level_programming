7b0d6da94edb58e226de4ba43b25aa9c1840906d
Aymen21916 <aymenhamidi111@gmail.com>
2023-11-30 01:53:49 -0800

    advanced 0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-s = 0
-for n in sys.argv[1:]:
-    s += int(n)
-print("{:d}".format(s))
+if __name__ == "__main__":
+    import sys
+    s = 0
+    for n in sys.argv[1:]:
+        s += int(n)
+    print("{:d}".format(s))
