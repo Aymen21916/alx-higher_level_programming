6ba1ce3dcbf0ec03c6a0923e403bbc8c9e2e08dd
Aymen21916 <aymenhamidi111@gmail.com>
2024-01-06 02:36:56 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,7 +12,18 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer.")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
         self.__width = width
+        
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer.")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = height
 
     def width(self):
